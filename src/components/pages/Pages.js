aa5e78c38268f123bc3fbc796d89d965676d67fa
rukmagat56 <rukmagat@codepixelzmedia.com.np>
2023-03-20 17:18:27 +0545

    react_wordpress

---
+++ src/components/pages/Pages.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../common/Navbar";
+import Home from "./Home";
+import Login from "./Login";
+import Posts from "./Posts";
+import Single from "./Single";
+import Logout from "./Logout";
+import Pagination from "./Posts/Pagination";
+const Pages = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<Single />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+      
+    </>
+  );
+};
+
+export default Pages;
